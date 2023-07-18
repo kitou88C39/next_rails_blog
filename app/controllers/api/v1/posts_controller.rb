46c432c746eb750250ea840a57c9cc93b374627a
kitou88C39 <kanfutrooper@icloud.com>
2023-07-18 09:18:43 +0900

    controller/api/v1/post_controller.rbを編集する

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,13 +1,37 @@
 class Api::V1::PostsController < ApplicationController
+  
+  # 投稿したブログを全て取得する
   def index
+    @posts = Post.all
+    render json: @posts
   end
-
+　
+  # 投稿したブログの固有ページを取得する
   def show
+    @post = Post.find(params[:id])
+    render json: @post
   end
 
+  # ブログを作成する
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render json: @post, status: :created
+    else
+      render json: @post.:errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
   end
 
   def destroy
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title,:content)
+  end  
 end
